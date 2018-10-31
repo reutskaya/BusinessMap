ffcaaf461199d38e61bbc5a06e921b49e275dc03
reutskaya <32382007+reutskaya@users.noreply.github.com>
2018-10-31 12:24:05 +0300

    Initial commit

---
+++ src/main/java/com/example/BusinessMap/BusinessMapApplication.java
@@ -0,0 +1,12 @@
+package com.example.BusinessMap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BusinessMapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BusinessMapApplication.class, args);
+	}
+}
